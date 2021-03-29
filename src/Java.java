0d254af54167211c008d8c093dc044b449ae685b
Mjd Fares <48202666+almajd123@users.noreply.github.com>
2021-03-29 17:24:41 +0200

    Update Java.java

--- src/Java.java
+++ src/Java.java
@@ -1,22 +1,32 @@
 public class Java
+{
 
-{    public static void main(String[] args) {
-    for (int i = 1; i <= 30; i++) {
-        boolean kaOrChing = false;
-        if (i % 3 == 0) {
-            System.out.print("ka-");
-            kaOrChing = true;
-        }
-        if (i % 5 == 0) {
-            System.out.print("ching");
-            kaOrChing = true;
-        }
 
-        if (kaOrChing) {
-            System.out.println();
-        } else {
-            System.out.println(String.valueOf(i));
+
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 30; i++) {
+            boolean kaOrChing3 = true;
+            boolean kaOrChing5 = true;
+
+            if ((i % 3 == 0)&&(i%15!=0)) {
+                System.out.println("ka");
+                kaOrChing3=false;
+            }
+            if ((i % 5 == 0) &&(i%15!=0)){
+                System.out.println("ching");
+                kaOrChing5=false;
+
+            }
+
+            if (i%15==0) {
+                System.out.println(String.valueOf("ka-ching!"));
+
+            } else if ((kaOrChing3)&&(kaOrChing5)){
+                System.out.println(i);
+
+            }
         }
     }
-}
 }
